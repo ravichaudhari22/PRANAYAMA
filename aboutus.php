e28bc1a223a6301184ad424dfbd454f0dea91f57
manik6897 <74670285+manik6897@users.noreply.github.com>
2021-09-07 16:57:57 -0400

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="aboutus.css">
+    <link href="style.css" type="text/css" rel="Stylesheet" />
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.min.css" />
+    <script>
+    function myFunction() {
+      document.getElementById("member").innerHTML = "";
+    }
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://kit.fontawesome.com/95dc93da07.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+  <section id="hero">
+    <header>
+      <div class="container">
+        <div id="header-logo-mobile" class="top-gym-logo">
+          <a href="#">Pran<br/>aYam</a>
+        </div>
+  <nav>
+          <nav>
+            <div class="wrapper">
+              <ul>
+                <li><a   href="index.php">Home</li>
+                  </ul>
+            <button id="hamburger-menu">
+          <span class="strip"></span>
+          <span class="strip"></span>
+          <span class="strip"></span>
+        </button>
+      </div>
+    </div>
+      </div>
+    </header>
+
+    <div class="about">
+      <image src="bg3.jpg" style="width : 900;" height="400;" ></image>
+        <div class="inner-section">
+            <h1>About Us</h1>
+            <p class="text">
+The website “PRANAYAMA” provides a simple platform for the customers who want to make their
+ health happier with good physical and mental health without any stress by maintaining  regular diet plans.<br>
+ <br>
+ In the website we have different types of diets for example Keto,Vegan,Vegetarian and Non-vegetarian so that all the people who eat different types of dishes can access the videos and recepies.<br>
+ <br>
+ Along with that we also have videos of different types of workouts so that they can watch and learn at the same time.<br>
+ Our members can ask about any query regarding their mental or physical health to our admin and can get a fast and satisfying response from our admin.<br>
+ <br>
+ <span>Thank you for visiting our website.</span>
+            </p>
+
+        </div>
+    </div>
+
+    <div style="text-align:center">
+      <div class="card" style="width: 18rem;">
+      <img class="card-img-top" src="ravi.jpeg" style="width : 300;" height="300;" >
+      <div class="card-body">
+        <h4 class="card-title">Ravi Chaudhari</h4>
+
+        <div class="card" style="width: 18rem;">
+          <img class="card-img-top" src="sahil.png" style="width : 300;" height="300;">
+          <div class="card-body">
+            <h4 class="card-title">Sahil Mahanjan</h4>
+
+            <div style="
+          float: right;">
+  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="navya.png" style="width : 300;" height="300;">
+  <div class="card-body">
+    <h4 class="card-title">Navya Shri</h4>
+
+    <div class="card" style="width: 18rem;">
+      <img class="card-img-top" src="manik.png" style="width : 300;" height="300;" >
+      <div class="card-body">
+      <h4 class="card-title">Manik Wahi</h4>
+
+      </div>
+    </div>
+
+  </div>
+  </div>
+
+
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+
+  </div>
+
+
+
+  </div>
+
+
+
+
+
+</body>
+</html>
---
+++ calendar.php
@@ -0,0 +1,163 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="vegan.css">
+    <link href='http://fonts.googleapis.com/css?family=Noto+Sans:400,700' rel='stylesheet' type='text/css'>
+      <link href="style.css" type="text/css" rel="Stylesheet" />
+      <script>
+      function myFunction() {
+        document.getElementById("member").innerHTML = "";
+      }
+      </script>
+</head>
+<body>
+
+
+
+
+  <header>
+    <div class="container">
+      <div id="header-logo-mobile" class="top-gym-logo">
+        <a href="#">Pran<br/>aYam</a>
+      </div>
+
+      <nav>
+        <nav>
+          <div class="wrapper">
+            <ul style="margin: 0px -70px">
+              <li><a   href="home2.php">Home</li>
+
+              <li><a   href="calendar.html">Calendar</li>
+
+              <li><a style="font-size:18px"> Recipies</a>
+                <ul>
+                  <li><a style="font-size:18px" href="Vegan.html">Vegan</a></li>
+                  <li><a style="font-size:18px" href="vegetarian.html">Vegetarian</a></li>
+                  <li><a style="font-size:18px" href="non-veg.html">Non Vegetarian</a></li>
+                </ul>
+                <li><a style="font-size:18px"> Workouts</a>
+                  <ul>
+                    <li><a style="font-size:18px" href="new.html">Yoga</a></li>
+                    <li><a style="font-size:18px" href="med.html">Meditation</a></li>
+                    <li><a style="font-size:18px" href="lift.html">Liftig</a></li>
+
+                  </ul>
+
+                    <li><a style="font-size:18px" href="donate.php">Donations</a></li>
+                      <li><a style="font-size:18px" href="Products.html">Products</a></li>
+
+                  <li><a style="font-size:18px "href="aboutus.html">About us</a></li>
+                <li style="float:right;margin:-31px">
+  <a href="javascript:void(1);" class="icon" onclick="myFunction()">
+      <i class="fa fa-bars"></i>
+    </a>
+</li>
+
+
+
+  </header>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Event Calendar</title>
+    <link rel="stylesheet" href="calendar.css" />
+  </head>
+  <body>
+    <div class="event-container">
+
+      <h3 class="year">Year 2021</h3>
+
+      <div class="event">
+        <div class="event-left">
+          <div class="event-date">
+            <div class="date">1</div>
+            <div class="month">Sep</div>
+          </div>
+        </div>
+
+        <div class="event-right">
+          <h3 class="event-title">Group Yoga</h3>
+
+          <div class="event-description">
+            Yoga is to be taught and our teachers will make sure you go home having a relaxed mind.
+            Anyone interested can join. :)
+          </div>
+
+          <div class="event-timing">
+            <img src="images/time.png" alt="" /> 10:00 am
+          </div>
+        </div>
+      </div>
+
+      <div class="event">
+        <div class="event-left">
+          <div class="event-date">
+            <div class="date">8</div>
+            <div class="month">Sep</div>
+          </div>
+        </div>
+
+        <div class="event-right">
+          <h3 class="event-title">Aerobics</h3>
+
+          <div class="event-description">
+            This class is a total body, heart pumping, aerobic and strength conditioning workout.
+          </div>
+
+          <div class="event-timing">
+            <img src="images/time.png" alt="" /> 10:30 am
+          </div>
+        </div>
+      </div>
+
+      <div class="event">
+        <div class="event-left">
+          <div class="event-date">
+            <div class="date">14</div>
+            <div class="month">Sep</div>
+          </div>
+        </div>
+
+        <div class="event-right">
+          <h3 class="event-title">Meditation</h3>
+
+          <div class="event-description">
+            This class will focus on slow, deep breathing techniques and meditative postures.
+          </div>
+
+          <div class="event-timing">
+            <img src="images/time.png" alt="" /> 10:00 am
+          </div>
+        </div>
+      </div>
+
+      <div class="event">
+        <div class="event-left">
+          <div class="event-date">
+            <div class="date">20</div>
+            <div class="month">Sep</div>
+          </div>
+        </div>
+
+        <div class="event-right">
+          <h3 class="event-title">Static Stretching</h3>
+
+          <div class="event-description">
+            Static stretching, on the other hand, is done at the end of your workout, and involves stretches that you hold in place for a period of time, without movement.<br>
+             This allows your muscles to loosen up, while increasing flexibility and range of motion.
+          </div>
+
+          <div class="event-timing">
+            <img src="images/time.png" alt="" /> 10:00 am
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
